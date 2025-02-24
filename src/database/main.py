9e66cd33c929e00e6030c8884e9bf493978220fc
Franck Tameze <francktameze5@gmail.com>
2025-02-24 22:24:03 +0100

    init a database

---
+++ src/database/main.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncEngine
+from src.config import Config
+from sqlmodel import SQLModel
+from src.books.models import Book
+
+
+engine = AsyncEngine(create_engine(
+    url=Config.DATABASE_URL,
+    echo=True
+))
+
+async def initdb():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
