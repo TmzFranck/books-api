0448daa4156130a0dc8b0c1ce51b996c40ac73b7
Franck Tameze <francktameze5@gmail.com>
2025-02-24 22:25:24 +0100

    add a asynccontextmanger to init a db on server start

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.database.main import initdb
 
 version = "v1"
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await initdb()
+    yield
+    print("server is stopping")
 
 app = FastAPI(
+    lifespan=lifespan,
     title="Books API",
     description="A RESTful API for a book review web service",
      version=version,
