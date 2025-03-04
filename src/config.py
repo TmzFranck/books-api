bedc460dc36cc09b286fb4222cdab1ac76f0e48b
Franck Tameze <francktameze5@gmail.com>
2025-03-04 20:21:07 +0100

    change .env directory and add .env.example

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str = ""
     REDIS_PORT: int = 0
     model_config = SettingsConfigDict(
-        env_file="src/.env", env_file_encoding="utf-8", extra="ignore"
+        env_file="./.env", env_file_encoding="utf-8", extra="ignore"
     )
 
 
